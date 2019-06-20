896006ff9810d2fb5c47d3508239ba08641e96d6
gw_zhangj2355 <gw_zhangj2355@sanyvdr.com>
2019-06-20 18:01:32 +0800

    项目搭建

--- src/main/java/com/owner/salary/record/OwnerApplication.java
+++ src/main/java/com/owner/salary/record/OwnerApplication.java
@@ -1,22 +1,12 @@
 package com.owner.salary.record;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 public class OwnerApplication {
-
     public static void main(String [] args){
         SpringApplication.run(OwnerApplication.class,args);
     }
-
-    @RequestMapping("/index")
-    public String index(){
-        return "hello boot";
-    }
-
 }
